37157b15e6385f731ff3d9a48955c56c8bc65269
Max Hochstein-Mintzel <maxhochsteinmintzel@gmail.com>
2025-09-03 16:07:01 +0100

    GCP first draft

---
+++ MUSHROOM/params.py
@@ -0,0 +1,9 @@
+import os
+
+##################  VARIABLES  ##################
+
+
+
+
+##################  CONSTANTS  #####################
+LOCAL_MODEL_PATH = os.path.join(os.path.expanduser('~'), "code", "yves-rdlb", "What-is-this-Mushroom")
